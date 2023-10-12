44da8f011cb9df40bc6ec824cc00b8614f0aecee
Lautaro <lautaroforte@protonmail.com>
2023-10-12 12:44:44 -0300

    terra

--- IaC/ec2-web.tf
+++ IaC/ec2-web.tf
@@ -1,26 +1,17 @@
-provider "aws" {
-  region = "us-east-1"  # Cambia la región según tu preferencia
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "single-instance"
+
+  instance_type          = "t2.micro"
+  ami = 
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
-
-resource "aws_instance" "ec2_instance" {
-  ami           = "ami-06db4d78cb1d3bbf9"  # ID de la AMI que deseas usar debian12
-  instance_type = "t2.micro"               # Tipo de instancia
-  key_name      = "nombre_de_tu_clave"     # Nombre de la clave SSH existente en AWS
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update -y
-              sudo apt-get install wget unzip -y
-              sudo apt-get install docker.io -y
-              sudo apt-get install git -y
-              sudo groupadd docker
-              sudo usermod -aG docker admin
-              sudo newgrp docker
-              sudo mkdir sitio
-              cd /sitio/
-              sudo git clone https://github.com/lauch4/lautaro-landing.git
-              sudo docker run -d --name apache-lautaro -p 80:80 -v /sitio/lautaro-landing/codigo:/usr/local/apache2/htdocs/ httpd:2.4
-              EOF
-}
-
-# Puedes agregar recursos adicionales aquí, como un grupo de seguridad, clave SSH, etc.
