a73fa472f50f7514801bf14efc9795d1cc30a878
Lautaro <lautaroforte@protonmail.com>
2023-10-17 14:41:19 -0300

    update ignore

--- IaC/ec2-web.tf
+++ IaC/ec2-web.tf
@@ -1,16 +1,29 @@
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+provider "aws" {
+  region = "us-east-1" # Asegúrate de ajustar la región según tus preferencias
+}
 
-  name = "single-instance"
+resource "aws_instance" "example" {
+  ami           = "ami-06db4d78cb1d3bbf9" # AMI de Debian 12 en la región us-east-1
+  instance_type = "t2.micro"
+  key_name      = "tu-par-de-claves" # Sustituye por tu par de claves
+  subnet_id     = "subnet-xxxxxxxx" # Sustituye por la ID de tu subred pública
 
-  instance_type          = "t2.micro"
-  ami                    = "ami-06db4d78cb1d3bbf9"
-  key_name               = "web-victus"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-0090e4169f256dc56"]
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install wget unzip -y
+              sudo apt-get install docker.io -y
+              sudo apt-get install git -y
+              sudo groupadd docker
+              sudo usermod -aG docker admin
+              sudo newgrp docker
+              sudo mkdir /sitio/
+              cd /sitio/
+              sudo git clone https://github.com/lauch4/lautaro-landing.git
+              sudo docker run -d --name apache-lautaro -p 80:80 -v /sitio/lautaro-landing/codigo:/usr/local/apache2/htdocs/ httpd:2.4
+              EOF
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "MiInstanciaEC2"
   }
 }
